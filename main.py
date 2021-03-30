792e3b3963a211479ce113725be785c475dcf6ae
Nishi Poddar <nishipoddar13@gmail.com>
2021-03-30 23:13:45 +0530

    Login backend

--- main.py
+++ main.py
@@ -1,25 +1,83 @@
-from flask import Flask,render_template,request,url_for,redirect
+from flask import Flask,render_template,request,url_for,redirect,session
+import re
+import mysql.connector
 app=Flask(__name__)
-
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="nineten910",
+  database="BOOKING"
+)
+cursor = mydb.cursor()
+app.secret_key = 'hello123'
 account={'username':"Nishi","Password":"Hello","email":"nishi@gmail.com"}
 @app.route("/")
 @app.route("/filmsy")
 @app.route("/home")
 def index():
     return render_template("index.html")
-
 @app.route("/profile")
 def profile():
-    return render_template("profile.html",account=account)
+    if session['loggedin']==True:
+        return render_template("profile.html",account=session)
+    else:
+        return render_template("login.html")
 
 @app.route("/book")
 def book():
     return render_template("book.html")
-
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
-    return render_template("login.html")
-
+    msg=''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        username = request.form['email']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM customer WHERE email_id = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['name'] = account[1]
+            session['username'] = account[0]
+            return render_template("index.html")
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template("login.html",msg=msg)
+@app.route("/register",methods=['POST'])
+def register():
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form and 'age' in request.form and 'phone' in request.form:
+        # Create variables for easy access
+        username = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        age=request.form['age']
+        phone=request.form['phone']
+        cursor.execute('SELECT * FROM customer WHERE email_id = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', username):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', name):
+            msg = 'Username must contain only characters and numbers!'
+        elif len(phone)!=9:
+            msg='Please enter 9 digit phone number'
+        elif not username or not password or not name:
+            msg = 'Please fill out the form!'
+        else:
+            age=int(age)
+            phone=int(phone)
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO customer VALUES (%s, %s, %s,%s,%s)', (username,name, password,phone,age))
+            mydb.commit()
+            return render_template("index.html")
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('login.html', msg=msg)
 if __name__ == "__main__":
     app.run(debug=True)
 
